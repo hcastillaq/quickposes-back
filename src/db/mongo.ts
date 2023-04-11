509b4e04f3797c4d47ea5f343fc802215e7abeb3
hernan castilla <hcastillaq@gmail.com>
2023-04-11 10:23:20 -0500

    fix db

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const uri =
-	'mongodb+srv://hcastillaq:true1996@cluster0.gaycnz8.mongodb.net/db?retryWrites=true&w=majority';
+	"mongodb+srv://hcastillaq:true1996@cluster0.gaycnz8.mongodb.net/db?retryWrites=true&w=majority";
 
 export const connectDB = () => {
 	mongoose
 		.connect(uri)
-		.then(() => console.info('Connected to MongoDB'))
+		.then(() => console.info("Connected to MongoDB"))
 		.catch((err) => console.error(err));
 };
